30efe78381cab6e0a30cc4a4340b0b8adb30a26e
Saphinite <Saphinite@gmail.com>
2018-10-26 17:02:48 -0400

    Changed

--- bash/cmdid.sh
+++ bash/cmdid.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 command=$1
-
-if [ $command = "" ]; then
-  read -p "Please enter a command name :" command
-  if [ $command = "" ]; then
+#Help
+case $1 in
+  -h | --help ) #sets the help command
+    echo "Enter a filename to recive the source"
+    exit 0 #exits with no error
+    ;;
+esac
+#Initial prompts
+  if [[ $command = "" ]]; then
+    read -p "Please enter a command name :" command
+    if [ $command = "" ]; then
+      exit 0
+    fi
+  fi
+#looped prompts
+while [[ $command != "" ]]; do
+  arg="dpkg -S  $command" #variable for the command to be run
+  $arg || echo "Not a file" #error checking
+  read -p " Please enter another command name : " command #reprompt
+  if [[ $command = "" ]]; then #exit conditions
     exit 0
   fi
-fi
-arg="dpkg -S  $command"
-echo $arg
-$arg
+done
